02683c7bbde21666d7f8c12a7171a81257c8d508
aamirkhan2478 <aamir.khan2478@gmail.com>
2023-07-13 16:15:48 +0500

    Create home and profile screen and add data dynamically

--- app/index.js
+++ app/index.js
@@ -1,14 +1,62 @@
-import { useEffect } from "react";
-import { SafeAreaView, Text } from "react-native";
-import { loadUser } from "../redux/Auth/authSlice";
-import { useDispatch } from "react-redux";
+import { ScrollView, StyleSheet, Text, View, Animated } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { showCategories } from "../redux/Category/categorySlice";
+import AnimatedLoader from "react-native-animated-loader";
+import { featuredProduct, showProducts } from "../redux/Product/productSlice";
+import Categories from "../components/Categories";
+import FeaturedProducts from "../components/FeaturedProducts";
+import NewProducts from "../components/NewProducts";
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const { cateLoading, categories } = useSelector((state) => state.category);
+  const {
+    productLoading,
+    featuredProducts: featuredProd,
+    products,
+  } = useSelector((state) => state.product);
+
+  useEffect(() => {
+    const loadCategories = () => dispatch(showCategories());
+    const featuredProducts = () => dispatch(featuredProduct());
+    const getProducts = () => dispatch(showProducts());
+
+    loadCategories();
+    featuredProducts();
+    getProducts();
+  }, []);
+
   return (
-    <SafeAreaView>
-      <Text>Home</Text>
-    </SafeAreaView>
+    <View style={styles.mainContainer}>
+      <AnimatedLoader
+        overlayColor='rgba(255,255,255,0.75)'
+        speed={1}
+        visible={cateLoading || productLoading}
+      />
+      <ScrollView>
+        <Text style={styles.sectionTitle}>Categories</Text>
+        <Categories categories={categories} />
+        <Text style={styles.sectionTitle}>Featured Products</Text>
+        <FeaturedProducts featuredProducts={featuredProd} />
+        <Text style={styles.sectionTitle}>New Products</Text>
+        <NewProducts items={products} />
+      </ScrollView>
+    </View>
   );
 };
 
 export default Home;
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+  },
+  sectionTitle: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "black",
+    marginHorizontal: 5,
+    marginTop: 10,
+  },
+});
--- app/profile.js
+++ app/profile.js
@@ -1,9 +1,18 @@
-import { View, Text } from "react-native";
 import React from "react";
 import Card from "../components/Card";
+import { useSelector } from "react-redux";
+import AnimatedLoader from "react-native-animated-loader";
 const profile = () => {
+  const { authLoading } = useSelector((state) => state.auth);
   return (
-    <Card/>
+    <>
+      <AnimatedLoader
+        overlayColor='rgba(255,255,255,0.75)'
+        speed={1}
+        visible={authLoading}
+      />
+      <Card />
+    </>
   );
 };
 
