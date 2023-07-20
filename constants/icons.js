9fd3da818806a3888be972c5d70a9015420f833d
aamirkhan2478 <aamir.khan2478@gmail.com>
2023-07-20 14:47:57 +0500

    Add constants

---
+++ constants/icons.js
@@ -0,0 +1,23 @@
+import search from "../assets/icons/search.png";
+import home from "../assets/icons/home.png";
+import dashboard from "../assets/icons/dashboard.png";
+import addProduct from "../assets/icons/add-product.png";
+import products from "../assets/icons/show-products.png";
+import addCategory from "../assets/icons/add-category.png";
+import categories from "../assets/icons/show-categories.png";
+import signup from "../assets/icons/signup.png";
+import login from "../assets/icons/login.png";
+import logout from "../assets/icons/shutdown.png";
+
+export default {
+  search,
+  home,
+  dashboard,
+  addProduct,
+  addCategory,
+  products,
+  categories,
+  signup,
+  login,
+  logout,
+};
