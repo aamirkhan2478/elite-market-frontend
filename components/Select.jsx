d70849367d05ba61d252ebf8f8a36c01da25e27f
aamirkhan2478 <aamir.khan2478@gmail.com>
2023-07-20 14:42:03 +0500

    Create a select field for showing different items

---
+++ components/Select.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+import { Picker } from "@react-native-picker/picker";
+
+const SelectDropdown = ({ options, setValues, values }) => {
+  const handleOptionSelect = (option) => {
+    setValues({ ...values, category: option });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Picker
+        selectedValue={values.category}
+        style={styles.dropdown}
+        onValueChange={(itemValue) => handleOptionSelect(itemValue)}
+      >
+        <Picker.Item label='Select an option' value='' />
+        {options?.map((option, index) => (
+          <Picker.Item
+            key={option._id}
+            label={option.name}
+            value={option._id}
+          />
+        ))}
+      </Picker>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+  dropdown: {
+    width: "100%",
+    borderWidth: 1,
+    borderColor: "black",
+  },
+});
+
+export default SelectDropdown;
